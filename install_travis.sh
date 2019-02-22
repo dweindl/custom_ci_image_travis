8868aa5a49f20c4b946c58242c92275fa4a667e8
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-02-22 10:54:50 +0100

    initial commit

---
+++ install_travis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "================= Installing travis CLIs ================="
+
+apt install ruby ruby-dev ruby-bundler
+
+cd
+git clone git://github.com/travis-ci/travis-build
+cd travis-build
+gem install travis
+gem install bundler
+bundle install
+bundler add travis
+bundler binstubs travis
+
+cd
+git clone https://github.com/travis-ci/travis-support.git
+cd travis-support
+gem build travis-support.gemspec
+gem install travis-support
+
+cd
+git clone https://github.com/travis-ci/travis-github_apps.git
+cd travis-github_apps/
+gem build travis-github_apps.gemspec
+gem install travis-github_apps
+
+cd
+git clone https://github.com/travis-ci/travis-rollout.git
+cd travis-rollout/
+gem build travis-rollout.gemspec
+gem install travis-rollout
+cd
+
+echo "Installed travis CLIs successfully"
+echo "-------------------------------------"
+
+echo "================= Cleaning package lists ==================="
+apt-get clean
+apt-get autoclean
+apt-get autoremove
