785ae485346f53bac2108e9c8feee9d511f8d62a
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-02-22 11:55:01 +0100

    ..

--- install_travis.sh
+++ install_travis.sh
@@ -10,29 +10,30 @@ cd
 git clone git://github.com/travis-ci/travis-build
 cd travis-build
 gem install travis
-gem install bundler
 bundle install
 bundler add travis
 bundler binstubs travis
+mkdir -p ~/.travis
+ln -s `pwd` ~/.travis/build
 
-cd
-git clone https://github.com/travis-ci/travis-support.git
-cd travis-support
-gem build travis-support.gemspec
-gem install travis-support
-
-cd
-git clone https://github.com/travis-ci/travis-github_apps.git
-cd travis-github_apps/
-gem build travis-github_apps.gemspec
-gem install travis-github_apps
-
-cd
-git clone https://github.com/travis-ci/travis-rollout.git
-cd travis-rollout/
-gem build travis-rollout.gemspec
-gem install travis-rollout
-cd
+#cd
+#git clone https://github.com/travis-ci/travis-support.git
+#cd travis-support
+#gem build travis-support.gemspec
+#gem install travis-support
+#
+#cd
+#git clone https://github.com/travis-ci/travis-github_apps.git
+#cd travis-github_apps/
+#gem build travis-github_apps.gemspec
+#gem install travis-github_apps
+#
+#cd
+#git clone https://github.com/travis-ci/travis-rollout.git
+#cd travis-rollout/
+#gem build travis-rollout.gemspec
+#gem install travis-rollout
+#cd
 
 echo "Installed travis CLIs successfully"
 echo "-------------------------------------"
